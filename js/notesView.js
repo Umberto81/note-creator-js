806ded69a4f381b00b6c8c42b3d6c9a959d7d5f2
Umberto81 <goffredo.gonnella@gail.com>
2022-04-16 12:47:34 +0100

    updated CRUD functions

--- js/notesView.js
+++ js/notesView.js
@@ -41,5 +41,60 @@ export default class notesView {
     //TODO: add the note preview by default
   }
 
-  _createListItemHTML(id, title, body, updated) {}
+  _createListItemHTML(id, title, body, updated) {
+    const _MAX_BODY_LENGHT = 60;
+
+    return `
+      <div class="notes__list-item" data-note-id="${id}">
+            <div class="notes__small-title">${title}</div>
+            <div class="notes__small-body">${body.substring(
+              0,
+              _MAX_BODY_LENGHT
+            )}
+            ${body.length > 60 ? "..." : ""}
+            </div>
+            <div class="notes__small-updtated">${updated.toLocaleString(
+              undefined,
+              { dateStyle: "full", timeStyle: "short" }
+            )}</div>
+
+      </div>
+    `;
+  }
+
+  updateNoteList(notes) {
+    const notesListContainer = this.root.querySelector(".notes__list");
+    //clear the list
+    notesListContainer.innerHTML = "";
+
+    for (const note of notes) {
+      const html = this._createListItemHTML(
+        note.id,
+        note.title,
+        note.body,
+        new Date(note.updated)
+      );
+      notesListContainer.insertAdjacentHTML("beforeend", html);
+    }
+
+    //add select/delet event for each note item
+    notesListContainer
+      .querySelectorAll(".notes__list-item")
+      .forEach((noteListItem) => {
+        noteListItem.addEventListener("click", () => {
+          this.onNoteSelect(noteListItem.dataset.noteId);
+        });
+
+        noteListItem.addEventListener("dblclick", () => {
+          const doDelete = confirm("Are you shure you want to delete?");
+          if (doDelete) {
+            this.onNoteDelete(noteListItem.dataset.noteId);
+          }
+        });
+      });
+  }
+
+  updateActiveNote(note) {
+    this.root.querySelector();
+  }
 }
